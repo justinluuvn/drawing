9ecf9df5e6f73cd6a4419583e8380effa4867853
justinluuvn <37363724+justinluuvn@users.noreply.github.com>
2018-04-26 10:21:26 +0300

    Update ilines.c

--- ilines.c
+++ ilines.c
@@ -1,12 +1,17 @@
-#include <stdio.h>
+/*
+	This file contains the code for drawing two diagonal straight lines which intersect each other in their center.
+*/
+#include <stdio.h> //add preprocessors
 #include "drawing.h"
 
 void iLines(void)
 {
 	int i;
-	clearScreen();
+	clearScreen(); //clear the screen
+	//from the four end points of two lines, gradually extend the lines by adding character "*" toward the intersecting point
 	for(i=0;i<=20;i++)
 	{
+		//each loop cycle, four "*" are added 
 		gotoxy(1+i,1+2*i);
 		printf("*");
 		gotoxy(41-i,81-2*i);
@@ -15,8 +20,8 @@ void iLines(void)
                 printf("*");
 		gotoxy(1+i,81-2*i);
                 printf("*");
-		delay(DELAY);
+		delay(DELAY); //delay for 100ms
 	}
-	printf("\n");
-	sleep(5);
+	printf("\n"); //print a new line
+	sleep(5); //delay for 5 seconds
 }
