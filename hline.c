5f1be8af71594390c216fb16010133641e46ccd0
justinluuvn <37363724+justinluuvn@users.noreply.github.com>
2018-04-26 10:17:36 +0300

    Update hline.c

--- hline.c
+++ hline.c
@@ -1,16 +1,26 @@
-#include <stdio.h>
+/*
+	This file contains the code for drawing a straight horizontal line
+*/
+#include <stdio.h> //add preprocessors
 #include "drawing.h"
 
+/*
+	Function definition of hLine()
+	This function aims at drawing a series of "*" characters from the left to the right to create a straight line
+	Input argument: none
+	Return argument: none
+*/
 void hLine(void)
 {
 	int i;
-        clearScreen();
-        for(i=1; i<=60; i++)
+        clearScreen(); //clear the screen
+	//draw a single line on the 15th row from the left to the right 
+        for(i=1; i<=60; i++) 
         {
                 gotoxy(15, i);
                 printf("*");
-                delay(DELAY);
+                delay(DELAY); //delay for 100ms
         }
-        printf("\n");
-        sleep(5);
+        printf("\n"); //print a new line
+        sleep(5); //delay for 5 seconds
 }
